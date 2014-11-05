bb92ff19be35cb352e0a89786a58e6bb53121a7d
Nathan Jones <nathan@ncjones.com>
2014-11-05 21:59:43 +1300

    create docset idx file

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 BASE_DIR=$(cd $(dirname $0) && pwd)
-DOCS_DIR=$BASE_DIR/apache-ant.docset/Contents/Resources/Documents
-rm -rf $DOCS_DIR
+RES_DIR=$BASE_DIR/apache-ant.docset/Contents/Resources
+DOCS_DIR=$RES_DIR/Documents
+rm -rf $RES_DIR
 mkdir -p $DOCS_DIR
 cp -r $BASE_DIR/apache-ant/manual/* \
       $BASE_DIR/apache-ant.docset/Contents/Resources/Documents
-
+sqlite3 $BASE_DIR/apache-ant.docset/Contents/Resources/docSet.dsidx <<SQL
+  CREATE TABLE searchIndex(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT,
+    path TEXT
+  );
+  CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);
+SQL
 
